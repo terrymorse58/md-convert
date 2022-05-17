acc9cdd2ebfdd19991e35278de7410a29fdeb0ef
Terry Morse <tmorse@terrymorse.com>
2022-05-17 16:03:22 -0700

    match lazyload images by caption (convertNytimesImgs)

--- transforms/convertnytimesimgs.js
+++ transforms/convertnytimesimgs.js
@@ -2,30 +2,89 @@
 //   convert all nytimes images using the document's
 //   window.__preloadedData.initialState object
 
+/*
+Structure of a nytimes lazy image container:
+
+<figure>
+  <div>
+    <span>Image</span>
+    <div data-testid="lazy-image">
+      <div data-testid="lazyimage-container"></div>
+    </div>
+  </div>
+  <figcaption>
+    <span>(caption text goes here)</span>
+    <span>
+      <span>Credit...</span>
+      <span>(credit text goes here)</span>
+    </span>
+  </figcaption>
+</figure>
+
+
+ */
+
 /**
  * depth-first search specified element and its children for image containers
  * @param {HTMLElement} element
- * @return {HTMLElement[]}
+ * @return {{lazyContainer: HTMLElement, credit: string, caption: string}[]}
  */
-function findImageContainers (element) {
-  // console.log(`findImageContainers element:`, element);
+function findLazyImageContainers (element) {
+  // console.log(`findLazyImageContainers element:`, element);
 
-  // return <img> and <div data-testid="lazyimage-container"> elements
+  // return when <div data-testid="lazyimage-container"> is found
   if (
-    element.tagName === 'IMG' ||
-    (element.tagName === 'DIV' && element.dataset.testid === 'lazyimage-container')
+    element.tagName === 'DIV' &&
+    element.dataset.testid === 'lazyimage-container'
   ) {
-    // console.log(`  findImageContainers found image node:
-    // ${element.outerHTML}`);
-    return [element];
+
+    const domData = {
+      lazyContainer: element,
+      credit: undefined,
+      caption: undefined
+    };
+
+    // get the container's <figure> parent and look for credit and caption
+    const figure = element.closest('figure');
+    console.assert(Boolean(figure),
+      `findLazyImageContainers parent figure not found`);
+    if (!figure) { return [domData]; }
+
+    const figcaption = figure.querySelector('figcaption');
+    if (!figcaption) { return [domData]; }
+
+    const figSpans = [...figcaption.children]
+      .filter(el => el.tagName === 'SPAN');
+
+    // console.log(`\n  findLazyImageContainers figSpans: [`);
+    // figSpans.forEach(span => {
+    //   console.log(`    ${span.outerHTML}`);
+    // });
+    // console.log(`    ]`);
+
+    // the child of a figcaption's span is either a caption or a credit
+    figSpans.forEach(span => {
+      const spanChildren = [...span.children],
+        childrenCount = spanChildren.length;
+
+      if (childrenCount === 0) {
+        // no children means it's a caption
+        domData.caption = span.textContent;
+      } else if (childrenCount === 2) {
+        // two children means credit text is in second child
+        const spanCredit = spanChildren[1];
+        domData.credit = spanCredit.textContent;
+      }
+    });
+
+    return [domData];
   }
 
-  // element is not an image node, so search element's
-  // children for image nodes
+  // element is not lazy image container, so search element's children
   let imgEls = [];
   const children = [...element.children];
   for (const child of children) {
-    const childImgs = findImageContainers(child);
+    const childImgs = findLazyImageContainers(child);
     if (childImgs.length) {
       imgEls = imgEls.concat(childImgs);
     }
@@ -45,14 +104,14 @@ function looseJsonParse (objStr) {
 }
 
 /**
- * gather all the images from the initialState data
+ * extract all the image data from the initialState data
  * @param {Window} window
  * @param {HTMLDocument} document
- * @return {String[]}
+ * @return {{imageType, credit, legacyHtmlCaption, url}[]}
  */
-function getInitialStateImages (window, document) {
+function getInitialStateImageData (window, document) {
   // console.log(`initialStateImages()`);
-  let imgUrls = [], preloadObjectStr;
+  let imgData = [], preloadObjectStr;
 
   // find the <script> element that contains 'window.__preloadedData'
   const scripts = [...document.querySelectorAll('script')];
@@ -69,15 +128,15 @@ function getInitialStateImages (window, document) {
 
   console.assert(preloadObjectStr,
     `initialStateImages preloadObjectStr not found`);
-  if (!preloadObjectStr) {return imgUrls;}
+  if (!preloadObjectStr) {return imgData;}
 
   const preloadData = looseJsonParse(preloadObjectStr);
   const {initialState} = preloadData;
 
-  console.assert(initialState, `getInitialStateImages initialState not found`);
-  if (!initialState) { return imgUrls; }
+  console.assert(initialState, `getInitialStateImageData initialState not found`);
+  if (!initialState) { return imgData; }
 
-  // review every property in initalState, looking for image data
+  // evaluate every property in initalState, looking for image data
   const props = Object.entries(initialState);
   for (let iProp = 0, len = props.length; iProp < len; iProp++) {
     const [propName, propVal] = props[iProp];
@@ -88,6 +147,12 @@ function getInitialStateImages (window, document) {
       propVal.__typename === 'Image'
     ) {
       // console.log(`Image ${propName}: `, propVal);
+      const {imageType, credit, legacyHtmlCaption} = propVal;
+      const stateObj = {
+        imageType,
+        credit,
+        legacyHtmlCaption
+      };
 
       // find next prop that starts with 'ImageRendition:' - that's our image
       for (let jProp = iProp + 1; jProp < len; jProp++) {
@@ -97,14 +162,31 @@ function getInitialStateImages (window, document) {
           typeof jpVal.url !== 'undefined'
         ) {
           // console.log(`    found image URL:`, jpVal.url);
-          imgUrls.push(jpVal.url);
+          stateObj.url = jpVal.url;
+          imgData.push(stateObj);
           break;
         }
       }
     }
   }
 
-  return imgUrls;
+  return imgData;
+}
+
+// form a hash object of image data with prop names equal to caption text
+/**
+ *
+ * @param {{imageType, credit, legacyHtmlCaption, url}[]} imgData
+ * @return {{}}
+ */
+function hashImgDataByCaption (imgData) {
+  const hashData = {};
+  imgData.forEach(dataObj => {
+    if (!dataObj.legacyHtmlCaption) { return; }
+    const propName = dataObj.legacyHtmlCaption;
+    hashData[propName] = dataObj;
+  });
+  return hashData;
 }
 
 /**
@@ -126,46 +208,64 @@ function convertNytimesImgs (document,
   // console.log(`  convertNytimesImgs body:`, body);
 
   // find elements that are or contain images
-  let domImageEls = findImageContainers(body);
+  let lazyContainers = findLazyImageContainers(body);
 
-  subjects = domImageEls.map(element => {
-    return {element};
+  subjects = lazyContainers.map(cont => {
+    const {lazyContainer, credit, caption} = cont;
+    return {
+      lazyContainer,
+      credit,
+      caption
+    };
   });
 
-  const initStateSrcUrls = getInitialStateImages(window, document);
+  const initStateImages = getInitialStateImageData(window, document);
 
   console.assert(
-    initStateSrcUrls.length >= domImageEls.length,
+    initStateImages.length >= lazyContainers.length,
     `convertNytimesImgs too few initialState images found`
   );
-  if (initStateSrcUrls.length < domImageEls.length) {
+  if (initStateImages.length < lazyContainers.length) {
     return body;
   }
 
+  const imgDataHash = hashImgDataByCaption(initStateImages);
+
+  // console.log(`imgDataHash:`, imgDataHash);
+
   // add urls to subjects
   subjects.forEach((subject, index) => {
-    subject.url = initStateSrcUrls[index];
-  });
-
-  console.log(`subjects:`);
-  subjects.forEach(({element, url}) => {
-    console.log(`\n  element: ${element.outerHTML}\n  url: ${url}`);
-  });
-
-  // add src url to each subject element
-  subjects.forEach(({element, url}) => {
-    if (element.tagName === 'IMG') {
-      element.src = url;
-    } else if (
-      element.tagName === 'DIV'
-    ) {
-      // add child img to div
-      const img = document.createElement('img');
-      img.src = url;
-      element.appendChild(img);
-    } else {
-      console.error(`convertNytimesImgs unknown dom element:`, element);
+    const {caption} = subject,
+      imgData = caption && imgDataHash[caption];
+    if (imgData) {
+      subject.url = imgData.url;
     }
+  });
+
+  // add child img to each lazyload div
+  subjects.forEach(subject => {
+    const {lazyContainer, credit, caption, url} = subject;
+    if (!url) {
+      console.log(
+`convertNytimesImgs didn't find url for subject: {
+  lazyContainer: ${lazyContainer.outerHTML},
+  credit: '${credit}',
+  caption: '${caption}'
+ }`);
+      return;
+    }
+
+    if (lazyContainer.tagName !== 'DIV') {
+      console.error(
+        `convertNytimesImgs unknown dom lazyContainer (should be div): ` +
+        `${lazyContainer.outerHTML}`);
+      return;
+    }
+
+    // add to lazyload div a child img with src=url
+    const img = document.createElement('img');
+    img.src = url;
+    lazyContainer.appendChild(img);
   });
 
   return body;
