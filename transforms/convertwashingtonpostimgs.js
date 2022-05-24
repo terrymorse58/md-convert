c977c88c42dfbdce0c5ea2532c3d171f44fc5166
Terry Morse <tmorse@terrymorse.com>
2022-05-24 13:32:25 -0700

    convertWashingtonpostImgs - recursive search for images + debug option

--- transforms/convertwashingtonpostimgs.js
+++ transforms/convertwashingtonpostimgs.js
@@ -56,14 +56,34 @@ function findArticleImageContainers (element) {
 }
 
 /**
- * extract all the image data from the <script id="__NEXT_DATA__>
+ * recursively find 'content_elements' items where element.type === "image"
+ * @param {Object} contentEl
+ * @param {Set} imgsSet
+ */
+function findImagesInContentElements(contentEl, imgsSet) {
+
+  if (contentEl.type === 'image') {
+    imgsSet.add(contentEl);
+  }
+
+  const {content_elements} = contentEl;
+  if (!content_elements) {return;}
+
+  // search for image elements in this element's content_elements
+  for (const childEl of content_elements) {
+    findImagesInContentElements(childEl, imgsSet);
+  }
+}
+
+/**
+ * extract all image data (type: 'image') from the
+ * <script id="__NEXT_DATA__>
  * @param {Window} window
  * @param {HTMLDocument} document
  * @return {{url: string, caption: string}[]}
  */
 function getScriptImageData (window, document) {
   // console.log(`getScriptImageData()`);
-  let imgData = [];
 
   // obtain the script element with id="__NEXT_DATA__"
   const scriptEl = document.querySelector('script#__NEXT_DATA__');
@@ -90,144 +110,176 @@ function getScriptImageData (window, document) {
     return imgData;
   }
 
-  for (const imgEl of content_elements) {
-
-    // only interested in `type: "image"` elements
-    if (imgEl.type !== 'image') {continue;}
-
-    const {url, credits_caption_display} = imgEl;
-    const el = {
-      caption: credits_caption_display,
-      url
-    };
-    imgData.push(el);
+  // form set of all content elements where type == 'image'
+  const imagesSet = new Set();
+  for (const contentEl of content_elements) {
+    findImagesInContentElements(contentEl, imagesSet);
   }
 
+  // form imageData from set of content images
+  const imgData = [...imagesSet].map(cEl => {
+    const {url, credits_caption_display} = cEl;
+    return {
+      caption: credits_caption_display,
+      url
+    }
+  });
+
   return imgData;
+}
+
+
+/**
+ * remove an element's children (if any)
+ * @param {HTMLElement} element
+ */
+function removeElementChildren (element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+/**
+ * true if the first scriptImages url is contained in targetStr
+ * @param {String} targetStr
+ * @param {{url: string}[]} scriptImages
+ * @return {Boolean}
+ */
+function stringContainsAScriptImagesUrl (targetStr, scriptImages) {
+  return targetStr.includes(scriptImages[0].url);
+}
+
+/**
+ * add image + blockquote to specified container
+ * @param {HTMLDocument} document
+ * @param {HTMLElement} container
+ * @param {string} url
+ * @param {string} caption
+ */
+function addImageToContainer (
+  document,
+  container,
+  url,
+  caption
+) {
+  const img = document.createElement('img'),
+    blockquote = document.createElement('blockquote');
+
+  img.src = url;
+  img.alt = caption;
+  container.appendChild(img);
+
+  blockquote.textContent = caption;
+  container.appendChild(blockquote);
 }
 
 /**
  * convert washingtonpost images
  * @param {HTMLDocument} document
  * @param {undefined} [element]
- * @param {undefined} [conversion]
+ * @param {Boolean} debug
  * @param {Window} window
  * @return {*|string|string|HTMLElement|BodyInit|ReadableStream<Uint8Array>|null}
  */
 function convertWashingtonpostImgs (document,
                                     element = undefined,
-                                    conversion = undefined,
+                                    {debug = false},
                                     window) {
-  // console.log('convertWashingtonpostImgs()');
-  let subjects;
 
-  const body = document.body;
-  // console.log(`  convertWashingtonpostImgs body:`, body);
+  if (debug) {
+    console.log('convertWashingtonpostImgs()');
+  }
 
-  // find elements that are or contain images
+  const {body} = document;
+
+  // find HTML elements that may contain images
+
   let imgContainers = findArticleImageContainers(body);
 
-  subjects = imgContainers.map(cont => {
+  const htmlSubjects = imgContainers.map(cont => {
     const {imageContainer, containerType} = cont;
     return {
       imageContainer,
-      containerType,
-      imageData: undefined
+      containerType
     };
   });
 
-  {
-  //   console.log(`subjects:`);
-  //   let iSub = 0;
-  //   for (const sub of subjects) {
-  //     const {imageContainer, containerType} = sub;
-  //     console.log(
-  //       `  [${iSub++}]: {
-  //   imageContainer:
-  //     ${imageContainer.outerHTML},
-  //   containerType: '${containerType}'
-  // }`);
-  //   }
-  }
-
-  const scriptImages = getScriptImageData(window, document);
-
-  console.assert(
-    scriptImages.length >= imgContainers.length,
-    `convertWashingtonpostImgs too few initialState images found`
-  );
-  if (scriptImages.length < imgContainers.length) {
-    return body;
-  }
-
-  {
-  //   console.log(`scriptImages:`);
-  //   let iSImg = 0;
-  //   for (const sImg of scriptImages) {
-  //     const {url, caption} = sImg;
-  //     console.log(
-  //       `  [${iSImg++}]: {
-  //   url: '${url}',
-  //   caption: '${caption}'
-  // }`);
-  //   }
-  }
-
-  // remove an element's children (if any)
-  function removeElementChildren (element) {
-    while (element.firstChild) {
-      element.removeChild(element.firstChild);
+  if (debug) {
+    console.log(`  convertWashingtonpostImgs htmlSubjects:`);
+    let iSub = 0;
+    for (const sub of htmlSubjects) {
+      const {imageContainer, containerType} = sub;
+      console.log(
+        `  [${iSub++}]: {
+    imageContainer:
+      ${imageContainer.outerHTML},
+    containerType: '${containerType}'
+  }`);
     }
   }
 
-  // true if the first scriptImages url is contained in targetStr
-  function stringContainsAScriptImagesUrl (targetStr) {
-      return targetStr.includes(scriptImages[0].url);
+  // find image data in script
+
+  const scriptImages = getScriptImageData(window, document);
+
+  if (debug) {
+    console.log(`  convertWashingtonpostImgs scriptImages:`);
+    let iSImg = 0;
+    for (const sImg of scriptImages) {
+      const {url, caption} = sImg;
+      console.log(
+        `  [${iSImg++}]: {
+      url: '${url}',
+      caption: '${caption}'
+    }`);
+    }
   }
 
-  function addImageToContainer(container, url, caption) {
-    const img = document.createElement('img'),
-      blockquote = document.createElement('blockquote');
-
-    img.src = url;
-    img.alt = caption;
-    container.appendChild(img);
-
-    blockquote.textContent = caption;
-    container.appendChild(blockquote);
+  if (scriptImages.length < imgContainers.length) {
+    console.error(
+      `convertWashingtonpostImgs image count mismatch:
+      scriptImages.length:  ${scriptImages.length}
+      imgContainers.length: ${imgContainers.length}
+      SKIPPING IMAGE CONVERSION!`);
+    return body;
   }
 
-  // insert images and captions into subject elements
-  for (const subject of subjects) {
+  // insert images + captions into htmlSubject elements
+
+  for (const subject of htmlSubjects) {
     const {imageContainer, containerType} = subject;
 
     if (containerType === 'lede-art') {
+
+      // TERRY lede-art may contain a video instead of an image
+      // which we could possibly convert to an image
 
       // lede art may or may not have an image url in scriptData.
       // if it doesn't, skip it.
       // if it does, convert it.
       const ledeImg = imageContainer.querySelector('figure > div > img');
       if (!ledeImg) {
-        console.error(`addImageToContainer no no 'lede-art' image found!`);
         continue;
       }
+
       // see if the image's srcset contains one of the scriptImages urls
-      if (!stringContainsAScriptImagesUrl(ledeImg.srcset)) {
+      if (!stringContainsAScriptImagesUrl(ledeImg.srcset, scriptImages)) {
         // console.log(`addImageToContainer 'lede-art' image not in script images, skipping`);
         continue;
       }
+
       // image is in scriptImages, convert it
       // console.log(`addImageToContainer 'lede-art' image in script images, converting`);
       const {url, caption} = scriptImages.shift();
       removeElementChildren(imageContainer);
-      addImageToContainer(imageContainer, url, caption);
+      addImageToContainer(document, imageContainer, url, caption);
 
     } else if (containerType === 'article-image') {
 
       // single image with optional caption
       const {url, caption} = scriptImages.shift();
       removeElementChildren(imageContainer);
-      addImageToContainer(imageContainer, url, caption);
+      addImageToContainer(document, imageContainer, url, caption);
 
     } else if (containerType === 'article-image-group') {
 
@@ -235,8 +287,8 @@ function convertWashingtonpostImgs (document,
       let img1 = scriptImages.shift(),
         img2 = scriptImages.shift();
       removeElementChildren(imageContainer);
-      addImageToContainer(imageContainer, img1.url, img1.caption);
-      addImageToContainer(imageContainer, img2.url, img2.caption);
+      addImageToContainer(document, imageContainer, img1.url, img1.caption);
+      addImageToContainer(document, imageContainer, img2.url, img2.caption);
 
     } else {
       console.error(
@@ -244,18 +296,18 @@ function convertWashingtonpostImgs (document,
     }
   }
 
-  {
-  //   console.log(`subjects after conversion:`);
-  //   let iSub = 0;
-  //   for (const sub of subjects) {
-  //     const {imageContainer, containerType} = sub;
-  //     console.log(
-  //       `  [${iSub++}]: {
-  //   imageContainer:
-  //     ${imageContainer.outerHTML},
-  //   containerType: '${containerType}'
-  // }`);
-  //   }
+  if (debug) {
+    console.log(`  convertWashingtonpostImgs htmlSubjects after conversion:`);
+    let iSub = 0;
+    for (const sub of htmlSubjects) {
+      const {imageContainer, containerType} = sub;
+      console.log(
+        `  [${iSub++}]: {
+      imageContainer:
+        ${imageContainer.outerHTML},
+      containerType: '${containerType}'
+    }`);
+    }
   }
 
   return body;
