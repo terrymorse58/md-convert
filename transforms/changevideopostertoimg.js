5aab1501d3781c8368ab7890f54fee19d3bba58a
Terry Morse <tmorse@terrymorse.com>
2022-06-12 09:59:49 -0700

    changeVideoPosterToImg add link to video

--- transforms/changevideopostertoimg.js
+++ transforms/changevideopostertoimg.js
@@ -1,25 +1,63 @@
 // md-convert transform 'changeVideoPosterToImg'
 
+/**
+ *
+ * @param {HTMLDocument} document
+ * @param video
+ * @param debug
+ * @return {*}
+ */
 function changeVideoPosterToImg (
   document,
-  element
+  video,
+  {debug = false}
 ) {
-  // console.log(`changeVideoPosterToImg()`);
 
-  if (!element ||
-    element.tagName !== 'VIDEO' ||
-    !element.hasAttribute('poster')
-  ) {
-    console.error('ERROR changeVideoPosterToImg bad element:', element);
-    return element;
+  if (debug) {
+    console.log(`changeVideoPosterToImg()`);
+    console.log(`  changeVideoPosterToImg video:`, video.outerHTML);
   }
 
-  const srcStr = element.poster;
+  if (!video ||
+    video.tagName !== 'VIDEO' ||
+    !video.hasAttribute('poster')
+  ) {
+    console.error('ERROR changeVideoPosterToImg bad video:', video.outerHTML);
+    return video;
+  }
+
+  // container to surround image and possible anchor
+  const container = document.createElement('div');
+
+  // create image
+  let srcStr = video.poster;
+  if (srcStr.startsWith('//')) {
+    srcStr = 'https:' + srcStr;
+  }
   const img = document.createElement('img');
   img.src = srcStr;
   img.alt = 'converted video poster';
-  element.replaceWith(img);
-  return img;
+  container.appendChild(img);
+
+  // append link to video if source exists
+  const source = video.querySelector('source'),
+    href = source?.src;
+  if (href) {
+    const anchor = document.createElement('a');
+    anchor.href = href;
+    anchor.textContent = 'Play video';
+    container.appendChild(document.createElement('br'));
+    container.appendChild(anchor);
+  }
+
+  video.replaceWith(container);
+
+  if (debug) {
+    console.log(`  changeVideoPosterToImg complete, container:`,
+      container.outerHTML);
+  }
+
+  return container;
 }
 
 export { changeVideoPosterToImg };
